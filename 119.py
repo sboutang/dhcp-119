a685b8d582cf40297fbe7a649a686e30ff427538
Scott Boutang <8740452+sboutang@users.noreply.github.com>
2024-02-06 09:15:26 -0600

    Update systemd format

--- 119.py
+++ 119.py
@@ -20,9 +20,7 @@ def convert_domains_to_option_119(domains):
                 compressed_domains.extend([len(parts[i]), *[ord(char) for char in parts[i]]])
         else:
              compressed_domains.append(0x00)
-    compressed_hex = compressed_domains.hex().upper()
-    compressed_text = str(compressed_domains)[12:-2]
-    return (compressed_hex, compressed_text)
+    return (compressed_domains.hex().upper())
 
 def format_as_windows(hex_string):
     formatted_string = " ".join([hex_string[i:i+2] for i in range(0, len(hex_string), 2)])
@@ -36,18 +34,30 @@ def format_as_cisco(hex_string):
     formatted_string = ".".join([hex_string[i:i+4] for i in range(0, len(hex_string), 4)])
     return formatted_string
 
+def format_as_systemd(hex_string):
+    byte_array = bytearray.fromhex(hex_string)
+    first_number = byte_array[0]
+    decoded_text = f"\\x{first_number:02x}"
+    for byte in byte_array[1:]:
+        if byte < 32 or byte == 127:
+            decoded_text += f'\\x{byte:02x}'
+        else:
+            try:
+                decoded_text += byte.to_bytes(1, 'little').decode('utf-8')
+            except UnicodeDecodeError:
+                decoded_text += f'\\x{byte:02x}'
+    return(decoded_text)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("domain_input", nargs='+', help="Enter the domain name or names as space seperated. ex. google.com apple.com")
     args = parser.parse_args()
     domain_list = ' '.join(args.domain_input)
     print(f'\ndomain search list: {domain_list}\n')
-    option_119_hex, option_119_str = convert_domains_to_option_119(args.domain_input)
-    meraki_119_string = format_as_meraki(option_119_hex)
-    windows_119_string = format_as_windows(option_119_hex)
-    cisco_119_string = format_as_cisco(option_119_hex)
-    print(f'Meraki: {meraki_119_string}')
-    print(f'Cisco: {cisco_119_string}')
-    print(f'Windows: {windows_119_string}')
-    print(f'systemd-network: SendOption=119:string:{option_119_str}')
+    option_119_hex = convert_domains_to_option_119(args.domain_input)
+
+    print(f'Meraki: {format_as_meraki(option_119_hex)}')
+    print(f'Cisco: {format_as_cisco(option_119_hex)}')
+    print(f'Windows: {format_as_windows(option_119_hex)}')
+    print(f'systemd-network: SendOption=119:string:{format_as_systemd(option_119_hex)}')
     print(f'raw hex: {option_119_hex}')
